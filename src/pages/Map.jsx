3204b67a8e2cd0cbe374919ee5ff8ab49ed1d1d4
Barron <barronakn@gmail.com>
2023-05-23 17:48:21 +0100

    Mise à jour

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -5,7 +5,14 @@ const Map = () => {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <MapboxMap />
+      <div>
+        <h1 className="text-center text-4xl bg-gray-800 text-white ">
+          Carte du monde
+        </h1>
+        <div className="map">
+          <MapboxMap />
+        </div>
+      </div>
     </QueryClientProvider>
   );
 };
